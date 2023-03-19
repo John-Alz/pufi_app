f6d96bfda195b00543e9242c5a9ffc0d85627817
John Angel <99442406+John-Alz@users.noreply.github.com>
2023-03-19 10:08:42 -0500

    SeAgregoSweetAlert

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,83 @@ import escudo from '../../assets/escudo.png'
 import { FaFacebookF} from "react-icons/fa";
 import { FaTwitter} from "react-icons/fa";
 import { FaInstagram} from "react-icons/fa";
-// import './footer.css'
+import './footer.css'
 
 export default function Footer() {
   return (
-    <div>
+    <footer className="footer-sec">
+    <div className="main">
+      
+      
+      <div className="logo row">
+        <div className="footer-header">
+          <img src={logo} className="manik" alt=""/>
+        </div>
+        {/* <div className="logo-des">
+          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+          
+          <a href="#" className="btn-know">Know More</a>
+        </div> */}
+        
+        
+      </div>
+      
+      
+      
+      <div className="office row">
+        {/* <div className="footer-header">
+          <h3>Office</h3>
+        </div> */}
+        <div className="link-des">
+          <a href="#" className="footer-links">PUFI RAIN</a>
+          <a href="#" className="footer-links">PUFI PUFF</a>
+          <a href="#" className="footer-links">PUFI CART</a>
+          <a href="#" className="footer-links">PUFIN NAP</a>
+        </div>
+      </div>
+      
+      
+      <div className="link row">
+        {/* <div className="footer-header">
+          <h3>Links</h3>
+        </div> */}
+        
+        <div className="link-des">
+          <a href="#" className="footer-links">CONTACTO</a>
+          <a href="#" className="footer-links">AYUDA</a>
+          <a href="#" className="footer-links">CÓMO COMPRAR</a>
+          <a href="#" className="footer-links">TÉRMINOS & CONDICIONES</a>
+        </div>
+        
+      </div>
+      
+      
+      <div className="newsletter row">
+        <div className="footer-header">
+          <h3>Newsletter</h3>
+        </div>
+        <div className="newsletter-des">
+          {/* <div className="subcribe"><i className="sub-icon ri-mail-check-fill"></i>
+            <input type="mail" placeholder = "Enter Email ID" required/>
+            <i className="sub-icon ri-arrow-right-line"></i>
+          </div> */}
+          <div className="icons">
+            <a href="#"><i><FaFacebookF/></i></a>
+            <a href="#"><i><FaTwitter/></i></a>
+            <a href="#"><i><FaInstagram/></i></a>
+            
+          </div>
+        </div>
+      </div>
+      
       
     </div>
+    <div className="copyright">
+    <hr/>
+    
+    <p>PUFI Copyright 2023 - Todos los derachos reservados.</p>
+    </div>
+  </footer>
 
   )
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import { FiArrowRight} from "react-icons/fi";
+import Swal from 'sweetalert2'
+
+import { useForm, ValidationError  } from '@formspree/react';
+
 
 export default function Form() {
+
+  const [state, handleSubmit] = useForm("mpzejzkz");
+  // if (state.succeeded) {
+  //     return <p>Thanks for joining!</p>;
+  // }
+
+  if (state.succeeded) {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Thanks for joining!',
+      showConfirmButton: false,
+      color: '#000',
+      background: '#fff;',
+      timer: 2500
+    })
+    return;
+}
+
+   
   return (
     <div>
       <div className='container_newsletter'>
@@ -14,15 +38,22 @@ export default function Form() {
       <p>Y enterate de todas la navovedades</p>
       </div>
       <div className='container_form'>
-        <form>
+        <form onSubmit={handleSubmit}>
         <input
-        type="email"
+        type="text"
+        id="email" 
+        name="email"
         placeholder='Ingresa tu email'
         required
         />
-        <button type='submit' className='sent'>
+        <button type='submit' className='sent' disabled={state.submitting}>
         <FiArrowRight className='sent_arrow'/>
         </button>
+        <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
         </form>
       </div>
     </div>
